cabc5867bb2c33b0d14c7464cd51bd5e3f08b898
irusaf@yahoo.com <irusaf@yahoo.com>
2018-08-29 22:31:22 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,9 +2,6 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-    if (index.height === 70) {
-    console.log("The height is correct")
-} 
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
